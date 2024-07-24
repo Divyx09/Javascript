1bc385e09b11f391dbc44ead77e9a0fff18eed0f
Divyx09 <divyaattarde94@gmail.com>
2024-07-24 17:31:35 +0530

    todoList

--- TodoList.js
+++ TodoList.js
@@ -1,5 +1,40 @@
-const todoList = [];
+let todoList = [];
+const todoInput = document.getElementById("todo-input");
+const addTodoBtn = document.getElementById("add-todo-btn");
+const todoListElement = document.getElementById("todo-list");
 
-const items = () => {
-    
-}; 
+const addTodo = () => {
+  const todoText = todoInput.value;
+  if (todoText !== "") {
+    const todo = {
+      text: todoText,
+    };
+    todoList.push(todo);
+    console.log(todoList);
+    renderTodoList();
+    todoInput.value = "";
+  } else {
+    alert("Please enter a todo");
+  }
+};
+
+const renderTodoList = () => {
+  todoListElement.innerHTML = "";
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const todoElement = document.createElement("li");
+    todoElement.classList.add("todo-item");
+    todoElement.innerHTML = `
+            <span>${todo.text}</span>
+            <button class="delete-btn" onclick="deleteTodo(${i})">Delete</button>
+        `;
+    todoListElement.appendChild(todoElement);
+  }
+};
+
+const deleteTodo = (i) => {
+  todoList.splice(i, 1);
+  renderTodoList();
+};
+
+addTodoBtn.addEventListener("click", addTodo);
--- learn2.js
+++ learn2.js
@@ -47,6 +47,4 @@ for (let i = 1; i <= number; i++) {
   n1 = n2;
   n2 = nextTerm;
 }
-console.log(fibonacciSeries.toString())
-
-
+console.log(fibonacciSeries.toString());
